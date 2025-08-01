95b864dfdd480a259acde507a0d2e614706b5466
SOWON LEE <66356241+Leesowon@users.noreply.github.com>
2025-08-01 11:09:10 +0900

    feat: access_token 쿠키에서 유저 ID 추출하도록 인증 로직 수정

--- app/api/endpoint.py
+++ app/api/endpoint.py
@@ -1,16 +1,25 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Request, Depends
 from app.model.recommender import generate_recommendation_for_user
 from app.config.database import get_engine
 from app.data.loader import load_user_data, load_brand_data, load_user_brand_data, load_interaction_data, load_bookmark_data
 from app.features.builder import build_user_features
 from app.model.trainer import prepare_dataset, build_interactions, train_model
 from app.saver.db_saver import save_to_db
+from app.utils.auth import get_current_user_id_from_token
 
 router = APIRouter()
 
-@router.get("/recommend-on-demand/{user_id}")
-def recommend_on_demand(user_id: int):
+@router.get("/recommend-user")
+def recommend_on_demand(request: Request):
     try:
+        # 0. 쿠키에서 user_id 추출
+        try:
+            user_id = get_current_user_id_from_token(request)
+            print(f"[추천 API] AccessToken에서 추출된 user_id: {user_id}")
+        except Exception as e:
+            print(f"[ERROR] user_id 추출 실패: {e}")
+            raise HTTPException(status_code=401, detail="Access token이 유효하지 않습니다.")
+
         # 1. DB 연결
         engine = get_engine()
         with engine.connect() as conn:
@@ -31,7 +40,7 @@ def recommend_on_demand(user_id: int):
         # 4. 모델 학습
         model = train_model(interactions, weights, user_features)
 
-        # 3. 추천 생성
+        # 5. 추천 생성
         recommend_df = generate_recommendation_for_user(
             user_id, user_df, brand_df, model, dataset, user_features
         )
@@ -39,14 +48,16 @@ def recommend_on_demand(user_id: int):
         if recommend_df.empty:
             raise HTTPException(status_code=404, detail="추천할 브랜드가 없습니다.")
 
-        # 4. DB 저장
+        # 6. DB 저장
         save_to_db(engine, recommend_df)
 
-        # 5. 응답 반환
+        # 7. 응답 반환
         return {
             "user_id": user_id,
             "recommendations": recommend_df[["brand_id", "score", "rank"]].to_dict(orient="records")
         }
 
     except Exception as e:
+        import traceback
+        print("[ERROR]", traceback.format_exc())
         raise HTTPException(status_code=500, detail=str(e))
