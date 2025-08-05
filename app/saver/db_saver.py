fadb39e3043ec2e799af3179998fa4481e7aad33
SOWON LEE <66356241+Leesowon@users.noreply.github.com>
2025-08-05 14:31:23 +0900

    chore: 불필요한 주석 제거

--- app/saver/db_saver.py
+++ app/saver/db_saver.py
@@ -16,25 +16,7 @@ def save_statistics(engine, statistics_df):
     if statistics_df.empty:
         logger.warning("⚠️ 저장할 통계 데이터가 없습니다.")
         return
-
-    # statistics_records = []
-    # for row in statistics_df.itertuples(index=False):
-    #     if not all([row.brand_name, row.category_id, row.category_name]):
-    #         continue  # 누락된 정보가 있다면 저장하지 않음
-    #
-    #     statistics_records.append({
-    #         "user_id": row.user_id,
-    #         "my_map_list_id": None,
-    #         "store_id": None,
-    #         "brand_id": row.brand_id,
-    #         "brand_name": row.brand_name,
-    #         "category_id": row.category_id,
-    #         "category_name": row.category_name,
-    #         "statistics_type": "RECOMMENDATION",
-    #         "created_at": row.created_at,
-    #         "updated_at": row.updated_at,
-    #     })
-
+    
     statistics_records = statistics_df.to_dict('records')
 
     with engine.begin() as conn:
